cddad5ad64ad01e2e69e3c8532679a9572750664
Ashwini Krishnappa (MS/EXM12-XC) <Ashwini.Krishnappa@in.bosch.com>
2022-05-13 16:58:49 +0530

    updated stats function definition

--- stats.c
+++ stats.c
@@ -2,9 +2,27 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    int i;
+    float max = numberset[0];
+    float min = numberset[0];
+    float average = numberset[0];
+    for(i=1, i<setlength, i++)
+    {
+        if(numberset[i]<min)
+        {
+            min = numberset[i];
+        }
+        if(numberset[i]>max)
+        {
+            max = numberset[i];
+        }
+        average = (average + numberset[i])/2;
+    }
+
+    s.average = average;
+    s.min = min;
+    s.max = max;
+    return s;
 }
 
 int emailAlertCallCount = 0;
